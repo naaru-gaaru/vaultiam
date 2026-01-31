e9ee100aedfcf8b6b2866fb8653bbef710ec5bfa
naaru-gaaru <narayan.govindaraj@gmail.com>
2026-01-31 12:48:20 -0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
           {/* Logo + Tagline */}
           <Link to="/" className="flex flex-col justify-center">
             <img
-              src="/vaultiam-logo.svg"
+              src="/vaultiam-logo-no-tag.svg"
               alt="VaultIAM"
               className="h-13 w-auto"
             />
@@ -65,3 +65,4 @@ export default function Header() {
     </header>
   );
 }
+
