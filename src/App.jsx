b0fc25624bd866077da7adcf5ec895bb9f1f15c1
naaru-gaaru <narayan.govindaraj@gmail.com>
2026-01-31 11:49:25 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+
 import Home from "./pages/Home";
 import Capabilities from "./pages/Capabilities";
 import CaseStudies from "./pages/CaseStudies";
@@ -7,10 +9,12 @@ import Contact from "./pages/Contact";
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/capabilities" element={<Capabilities />} />
-      <Route path="/case-studies" element={<CaseStudies />} />
-      <Route path="/contact" element={<Contact />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/capabilities" element={<Capabilities />} />
+        <Route path="/case-studies" element={<CaseStudies />} />
+        <Route path="/contact" element={<Contact />} />
+      </Route>
     </Routes>
   );
 }
