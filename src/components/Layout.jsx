e4f953a17ecbf06bf184e9a030dcff226d87d63f
naaru-gaaru <narayan.govindaraj@gmail.com>
2026-02-02 10:44:21 -0500

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -16,3 +16,21 @@ export default function Layout() {
     </div>
   );
 }
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+
+export default function Layout() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+
+      {/* IMPORTANT: no padding/margin here */}
+      <main className="flex-1">
+        <Outlet />
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
