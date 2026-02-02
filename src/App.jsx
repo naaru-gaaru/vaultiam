984ee366aecae77544243c8e7a91ccb7a149ddee
naaru-gaaru <narayan.govindaraj@gmail.com>
2026-02-02 08:16:36 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,16 @@ import Layout from "./components/Layout";
 
 import Home from "./pages/Home";
 import Capabilities from "./pages/Capabilities";
-import CaseStudies from "./pages/CaseStudies";
+import CustomerStories from "./pages/CustomerStories";
 import Contact from "./pages/Contact";
 
 export default function App() {
   return (
     <Routes>
-      {/* Layout wraps all pages */}
       <Route element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="capabilities" element={<Capabilities />} />
-        <Route path="case-studies" element={<CaseStudies />} />
+        <Route path="customer-stories" element={<CustomerStories />} />
         <Route path="contact" element={<Contact />} />
       </Route>
     </Routes>
