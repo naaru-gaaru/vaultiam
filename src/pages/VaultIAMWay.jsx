9b64e257c4de022f9ee35690e73815ce681cdf0f
naaru-gaaru <narayan.govindaraj@gmail.com>
2026-02-04 11:36:30 -0500

    Update VaultIAMWay.jsx

--- src/pages/VaultIAMWay.jsx
+++ src/pages/VaultIAMWay.jsx
@@ -107,7 +107,7 @@ function MethodologyFlow() {
       num: "01", 
       label: "Assess", 
       desc: "Comprehensive identity risk mapping & gap analysis",
-      icon: <path d="M9 11l3 3L22 4"/><path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"/>
+      icon: <><path d="M9 11l3 3L22 4"/><path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"/></>
     },
     { 
       num: "02", 
